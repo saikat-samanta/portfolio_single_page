b0241d9516c3517a33489c1e9549783e81c7f864
saikat-samanta <saikatsamanta737@gmail.com>
2023-03-18 23:33:02 +0530

    refactor: :rocket: make js dirty

--- temp.js
+++ temp.js
@@ -1,4 +1,4 @@
-var _0x3ca8 = [
+var _0x37e4 = [
   "\x75\x73\x65\x20\x73\x74\x72\x69\x63\x74",
   "\x70\x6F\x72\x74\x66\x6F\x6C\x69\x6F\x5F\x6C\x6F\x67\x6F",
   "\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64",
@@ -20,7 +20,7 @@ var _0x3ca8 = [
   "\x72\x65\x61\x64\x79\x53\x74\x61\x74\x65",
   "\x74\x61\x72\x67\x65\x74",
   "\x63\x6F\x6D\x70\x6C\x65\x74\x65",
-  "\x25\x63\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u2588\u2584\u2588\u2003\u2588\u2580\u2588\u2003\u2588\u2591\u2588\u2003\x20\u2003\u2584\u2580\u2588\u2003\u2588\u2580\u2588\u2003\u2588\u2580\u2580\u2003\x20\u2003\u2588\u2591\u2588\u2003\u2588\u2003\u2588\u2580\u2580\u2003\u2588\u2591\u2588\u2591\u2588\u2003\u2588\u2003\u2588\u2584\u2591\u2588\u2003\u2588\u2580\u2580\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u2591\u2588\u2591\u2003\u2588\u2584\u2588\u2003\u2588\u2584\u2588\u2003\x20\u2003\u2588\u2580\u2588\u2003\u2588\u2580\u2584\u2003\u2588\u2588\u2584\u2003\x20\u2003\u2580\u2584\u2580\u2003\u2588\u2003\u2588\u2588\u2584\u2003\u2580\u2584\u2580\u2584\u2580\u2003\u2588\u2003\u2588\u2591\u2580\u2588\u2003\u2588\u2584\u2588\x20\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u2591\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2591\u2588\u2588\u2588\u2588\u2588\u2557\u2591\u2588\u2588\u2557\u2588\u2588\u2557\u2591\u2591\u2588\u2588\u2557\u2591\u2588\u2588\u2588\u2588\u2588\u2557\u2591\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2557\u2591\u2588\u2588\u2588\u2588\u2588\u2588\u2557\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2551\u2588\u2588\u2551\u2591\u2588\u2588\u2554\u255D\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D\u255A\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u255A\u2588\u2588\u2588\u2588\u2588\u2557\u2591\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2550\u255D\u2591\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\u2591\u2591\u2591\u2588\u2588\u2551\u2591\u2591\u2591\u2591\u255A\u255D\u255A\u2588\u2588\u2588\u2588\u2588\u2557\u2591\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u2591\u255A\u2550\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551\u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2588\u2588\u2557\u2591\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551\u2591\u2591\u2591\u2588\u2588\u2551\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u255A\u2550\u2550\u2550\u2588\u2588\u2557\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2551\u2591\u2591\u2588\u2588\u2551\u2588\u2588\u2551\u2588\u2588\u2551\u2591\u255A\u2588\u2588\u2557\u2588\u2588\u2551\u2591\u2591\u2588\u2588\u2551\u2591\u2591\u2591\u2588\u2588\u2551\u2591\u2591\u2591\u2591\u2591\u2591\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u255A\u2550\u2550\u2550\u2550\u2550\u255D\u2591\u255A\u2550\u255D\u2591\u2591\u255A\u2550\u255D\u255A\u2550\u255D\u255A\u2550\u255D\u2591\u2591\u255A\u2550\u255D\u255A\u2550\u255D\u2591\u2591\u255A\u2550\u255D\u2591\u2591\u2591\u255A\u2550\u255D\u2591\u2591\u2591\u2591\u2591\u2591\u255A\u2550\u2550\u2550\u2550\u2550\u255D\u2591\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2591\u2591\u2588\u2588\u2588\u2588\u2588\u2557\u2591\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2591\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2591\u2588\u2588\u2588\u2588\u2588\u2557\u2591\u2588\u2588\u2557\u2591\u2591\u2591\u2591\u2591\u2588\u2588\u2557\u2591\u2588\u2588\u2588\u2588\u2588\u2557\u2591\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2551\u2591\u2591\u2591\u2591\u2591\u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2551\u2591\u2591\u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2591\u2591\u2591\u2588\u2588\u2551\u2591\u2591\u2591\u2588\u2588\u2588\u2588\u2588\u2557\u2591\u2591\u2588\u2588\u2551\u2591\u2591\u2588\u2588\u2551\u2588\u2588\u2551\u2591\u2591\u2591\u2591\u2591\u2588\u2588\u2551\u2588\u2588\u2551\u2591\u2591\u2588\u2588\u2551\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u2588\u2588\u2554\u2550\u2550\u2550\u255D\u2591\u2588\u2588\u2551\u2591\u2591\u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2591\u2591\u2591\u2588\u2588\u2551\u2591\u2591\u2591\u2588\u2588\u2554\u2550\u2550\u255D\u2591\u2591\u2588\u2588\u2551\u2591\u2591\u2588\u2588\u2551\u2588\u2588\u2551\u2591\u2591\u2591\u2591\u2591\u2588\u2588\u2551\u2588\u2588\u2551\u2591\u2591\u2588\u2588\u2551\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u2588\u2588\u2551\u2591\u2591\u2591\u2591\u2591\u255A\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2551\u2591\u2591\u2588\u2588\u2551\u2591\u2591\u2591\u2588\u2588\u2551\u2591\u2591\u2591\u2588\u2588\u2551\u2591\u2591\u2591\u2591\u2591\u255A\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2551\u255A\u2588\u2588\u2588\u2588\u2588\u2554\u255D\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\u255A\u2550\u255D\u2591\u2591\u2591\u2591\u2591\u2591\u255A\u2550\u2550\u2550\u2550\u255D\u2591\u255A\u2550\u255D\u2591\u2591\u255A\u2550\u255D\u2591\u2591\u2591\u255A\u2550\u255D\u2591\u2591\u2591\u255A\u2550\u255D\u2591\u2591\u2591\u2591\u2591\u2591\u255A\u2550\u2550\u2550\u2550\u255D\u2591\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D\u255A\u2550\u255D\u2591\u255A\u2550\u2550\u2550\u2550\u255D\u2591\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x25\x63\x20\uD83E\uDD73\x20\uD83E\uDD73\x20\uD83E\uDD73",
+  "\x25\x63\x0D\x0A\x0D\x0A\x20\x20\x20\x20\x20\x20\u252C\x20\u252C\u250C\u2500\u2510\u252C\x20\u252C\x20\x20\u250C\u2500\u2510\u252C\u2500\u2510\u250C\u2500\u2510\x0D\x0A\x20\x20\x20\x20\x20\x20\u2514\u252C\u2518\u2502\x20\u2502\u2502\x20\u2502\x20\x20\u251C\u2500\u2524\u251C\u252C\u2518\u251C\u2524\x20\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\u2534\x20\u2514\u2500\u2518\u2514\u2500\u2518\x20\x20\u2534\x20\u2534\u2534\u2514\u2500\u2514\u2500\u2518\x0D\x0A\x20\x20\x20\x20\x20\x20\u252C\x20\x20\u252C\u252C\u250C\u2500\u2510\u252C\x20\u252C\u252C\u250C\u2510\u250C\u250C\u2500\u2510\x20\x20\x0D\x0A\x20\x20\x20\x20\x20\x20\u2514\u2510\u250C\u2518\u2502\u251C\u2524\x20\u2502\u2502\u2502\u2502\u2502\u2502\u2502\u2502\x20\u252C\x20\x20\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\u2514\u2518\x20\u2534\u2514\u2500\u2518\u2514\u2534\u2518\u2534\u2518\u2514\u2518\u2514\u2500\u2518\x20\x20\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0D\x0A\x20\x20\x20\x20\x20\x20\u2554\u2550\u2557\u250C\u2500\u2510\u252C\u252C\u250C\u2500\u250C\u2500\u2510\u250C\u252C\u2510\u250C\u2500\u2510\x20\x20\x20\x20\x20\x20\x0D\x0A\x20\x20\x20\x20\x20\x20\u255A\u2550\u2557\u251C\u2500\u2524\u2502\u251C\u2534\u2510\u251C\u2500\u2524\x20\u2502\x20\u2514\u2500\u2510\x20\x20\x20\x20\x20\x20\x0D\x0A\x20\x20\x20\x20\x20\x20\u255A\u2550\u255D\u2534\x20\u2534\u2534\u2534\x20\u2534\u2534\x20\u2534\x20\u2534\x20\u2514\u2500\u2518\x20\x20\x20\x20\x20\x20\x0D\x0A\x20\x20\x20\x20\x20\x20\u2554\u2550\u2557\u250C\u2500\u2510\u252C\u2500\u2510\u250C\u252C\u2510\u250C\u2500\u2510\u250C\u2500\u2510\u252C\x20\x20\u252C\u250C\u2500\u2510\x0D\x0A\x20\x20\x20\x20\x20\x20\u2560\u2550\u255D\u2502\x20\u2502\u251C\u252C\u2518\x20\u2502\x20\u251C\u2524\x20\u2502\x20\u2502\u2502\x20\x20\u2502\u2502\x20\u2502\x0D\x0A\x20\x20\x20\x20\x20\x20\u2569\x20\x20\u2514\u2500\u2518\u2534\u2514\u2500\x20\u2534\x20\u2514\x20\x20\u2514\u2500\u2518\u2534\u2500\u2518\u2534\u2514\u2500\u2518\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0D\x0A\x20\x20\x20\x20\x20\x20\x25\x63\x20\uD83E\uDD73\x20\uD83E\uDD73\x20\uD83E\uDD73",
   "\x63\x6F\x6C\x6F\x72\x3A\x20\x62\x6C\x75\x65",
   "\x66\x6F\x6E\x74\x2D\x73\x69\x7A\x65\x3A\x20\x33\x30\x70\x78",
   "\x69\x6E\x66\x6F",
@@ -96,84 +96,84 @@ var _0x3ca8 = [
   "\x6C\x6F\x67",
   "\x72\x65\x73\x65\x74",
 ];
-_0x3ca8[0];
-const logo = document[_0x3ca8[2]](_0x3ca8[1]);
-const navBar = document[_0x3ca8[4]](_0x3ca8[3]);
-const typeEl = document[_0x3ca8[4]](_0x3ca8[5]);
-const sections = document[_0x3ca8[7]](_0x3ca8[6]);
-const navLi = document[_0x3ca8[7]](_0x3ca8[8]);
-const footerYear = document[_0x3ca8[2]](_0x3ca8[9]);
-const contactForm = document[_0x3ca8[2]](_0x3ca8[10]);
-const formSubmitBtn = document[_0x3ca8[2]](_0x3ca8[11]);
-const formSubmitStatus = document[_0x3ca8[2]](_0x3ca8[12]);
-const slidesContainer = document[_0x3ca8[2]](_0x3ca8[13]);
-const slide = document[_0x3ca8[4]](_0x3ca8[14]);
-const prevButton = document[_0x3ca8[2]](_0x3ca8[15]);
-const nextButton = document[_0x3ca8[2]](_0x3ca8[16]);
-document[_0x3ca8[25]](_0x3ca8[17], (_0xa36axe) => {
-  if (_0xa36axe[_0x3ca8[19]][_0x3ca8[18]] === _0x3ca8[20]) {
-    console[_0x3ca8[24]](`${_0x3ca8[21]}`, _0x3ca8[22], _0x3ca8[23]);
+_0x37e4[0];
+const logo = document[_0x37e4[2]](_0x37e4[1]);
+const navBar = document[_0x37e4[4]](_0x37e4[3]);
+const typeEl = document[_0x37e4[4]](_0x37e4[5]);
+const sections = document[_0x37e4[7]](_0x37e4[6]);
+const navLi = document[_0x37e4[7]](_0x37e4[8]);
+const footerYear = document[_0x37e4[2]](_0x37e4[9]);
+const contactForm = document[_0x37e4[2]](_0x37e4[10]);
+const formSubmitBtn = document[_0x37e4[2]](_0x37e4[11]);
+const formSubmitStatus = document[_0x37e4[2]](_0x37e4[12]);
+const slidesContainer = document[_0x37e4[2]](_0x37e4[13]);
+const slide = document[_0x37e4[4]](_0x37e4[14]);
+const prevButton = document[_0x37e4[2]](_0x37e4[15]);
+const nextButton = document[_0x37e4[2]](_0x37e4[16]);
+document[_0x37e4[25]](_0x37e4[17], (_0x9df1xe) => {
+  if (_0x9df1xe[_0x37e4[19]][_0x37e4[18]] === _0x37e4[20]) {
+    console[_0x37e4[24]](`${_0x37e4[21]}`, _0x37e4[22], _0x37e4[23]);
   }
 });
-footerYear[_0x3ca8[26]] = new Date()[_0x3ca8[27]]();
-window[_0x3ca8[28]] = (_0xa36axf) => {
-  window[_0x3ca8[30]]({ top: 0, behavior: _0x3ca8[29] });
-  window[_0x3ca8[32]][_0x3ca8[31]] = _0x3ca8[33];
-  document[_0x3ca8[2]](_0x3ca8[37])[_0x3ca8[36]][_0x3ca8[35]](_0x3ca8[34]);
+footerYear[_0x37e4[26]] = new Date()[_0x37e4[27]]();
+window[_0x37e4[28]] = (_0x9df1xf) => {
+  window[_0x37e4[30]]({ top: 0, behavior: _0x37e4[29] });
+  window[_0x37e4[32]][_0x37e4[31]] = _0x37e4[33];
+  document[_0x37e4[2]](_0x37e4[37])[_0x37e4[36]][_0x37e4[35]](_0x37e4[34]);
 };
-AOS[_0x3ca8[40]]({
+AOS[_0x37e4[40]]({
   offset: 0,
   delay: 0,
   duration: 1000,
-  easing: _0x3ca8[38],
+  easing: _0x37e4[38],
   once: false,
   mirror: true,
-  anchorPlacement: _0x3ca8[39],
+  anchorPlacement: _0x37e4[39],
 });
-document[_0x3ca8[25]](_0x3ca8[41], (_0xa36axf) => {
-  const _0xa36ax10 = localStorage[_0x3ca8[43]](_0x3ca8[42]);
-  document[_0x3ca8[2]](_0x3ca8[45])[_0x3ca8[44]] =
-    JSON[_0x3ca8[46]](_0xa36ax10);
+document[_0x37e4[25]](_0x37e4[41], (_0x9df1xf) => {
+  const _0x9df1x10 = localStorage[_0x37e4[43]](_0x37e4[42]);
+  document[_0x37e4[2]](_0x37e4[45])[_0x37e4[44]] =
+    JSON[_0x37e4[46]](_0x9df1x10);
   setDarkMode();
 });
-window[_0x3ca8[47]] = () => {
-  let _0xa36ax11 = _0x3ca8[48];
-  sections[_0x3ca8[52]]((_0xa36ax12) => {
-    const _0xa36ax13 = _0xa36ax12[_0x3ca8[49]];
-    if (scrollY >= _0xa36ax13 - 1) {
-      _0xa36ax11 = _0xa36ax12[_0x3ca8[51]](_0x3ca8[50]);
+window[_0x37e4[47]] = () => {
+  let _0x9df1x11 = _0x37e4[48];
+  sections[_0x37e4[52]]((_0x9df1x12) => {
+    const _0x9df1x13 = _0x9df1x12[_0x37e4[49]];
+    if (scrollY >= _0x9df1x13 - 1) {
+      _0x9df1x11 = _0x9df1x12[_0x37e4[51]](_0x37e4[50]);
     }
   });
-  navLi[_0x3ca8[52]]((_0xa36ax14) => {
-    _0xa36ax14[_0x3ca8[36]][_0x3ca8[53]](_0x3ca8[34]);
+  navLi[_0x37e4[52]]((_0x9df1x14) => {
+    _0x9df1x14[_0x37e4[36]][_0x37e4[53]](_0x37e4[34]);
     if (
-      _0xa36ax14[_0x3ca8[51]](_0x3ca8[50]) ===
-      `${_0x3ca8[54]}${_0xa36ax11}${_0x3ca8[33]}`
+      _0x9df1x14[_0x37e4[51]](_0x37e4[50]) ===
+      `${_0x37e4[54]}${_0x9df1x11}${_0x37e4[33]}`
     ) {
-      _0xa36ax14[_0x3ca8[36]][_0x3ca8[35]](_0x3ca8[34]);
+      _0x9df1x14[_0x37e4[36]][_0x37e4[35]](_0x37e4[34]);
     }
   });
 };
 function initialization() {
-  logo[_0x3ca8[57]](_0x3ca8[55], _0x3ca8[56]);
-  navBar[_0x3ca8[36]][_0x3ca8[35]](...[_0x3ca8[58], _0x3ca8[59]]);
+  logo[_0x37e4[57]](_0x37e4[55], _0x37e4[56]);
+  navBar[_0x37e4[36]][_0x37e4[35]](...[_0x37e4[58], _0x37e4[59]]);
 }
 initialization();
 function setDarkMode() {
-  const _0xa36ax17 = document[_0x3ca8[2]](_0x3ca8[45]);
-  localStorage[_0x3ca8[60]](_0x3ca8[42], _0xa36ax17[_0x3ca8[44]]);
-  if (_0xa36ax17[_0x3ca8[44]] === true) {
-    logo[_0x3ca8[57]](_0x3ca8[55], _0x3ca8[61]);
-    navBar[_0x3ca8[36]][_0x3ca8[53]](...[_0x3ca8[58], _0x3ca8[59]]);
-    navBar[_0x3ca8[36]][_0x3ca8[35]](...[_0x3ca8[62], _0x3ca8[63]]);
-    document[_0x3ca8[64]][_0x3ca8[36]][_0x3ca8[35]](_0x3ca8[45]);
+  const _0x9df1x17 = document[_0x37e4[2]](_0x37e4[45]);
+  localStorage[_0x37e4[60]](_0x37e4[42], _0x9df1x17[_0x37e4[44]]);
+  if (_0x9df1x17[_0x37e4[44]] === true) {
+    logo[_0x37e4[57]](_0x37e4[55], _0x37e4[61]);
+    navBar[_0x37e4[36]][_0x37e4[53]](...[_0x37e4[58], _0x37e4[59]]);
+    navBar[_0x37e4[36]][_0x37e4[35]](...[_0x37e4[62], _0x37e4[63]]);
+    document[_0x37e4[64]][_0x37e4[36]][_0x37e4[35]](_0x37e4[45]);
   } else {
-    navBar[_0x3ca8[36]][_0x3ca8[53]](...[_0x3ca8[62], _0x3ca8[63]]);
+    navBar[_0x37e4[36]][_0x37e4[53]](...[_0x37e4[62], _0x37e4[63]]);
     initialization();
-    document[_0x3ca8[64]][_0x3ca8[36]][_0x3ca8[53]](_0x3ca8[45]);
+    document[_0x37e4[64]][_0x37e4[36]][_0x37e4[53]](_0x37e4[45]);
   }
 }
-const contentArr = [_0x3ca8[65], _0x3ca8[66], _0x3ca8[67]];
+const contentArr = [_0x37e4[65], _0x37e4[66], _0x37e4[67]];
 let textString;
 let flag = false;
 let index = 0;
@@ -181,10 +181,10 @@ let speed = 400;
 let i = 0;
 const typing = () => {
   if (flag) {
-    textString = contentArr[index][_0x3ca8[68]](0, i);
-    typeEl[_0x3ca8[69]] = textString;
+    textString = contentArr[index][_0x37e4[68]](0, i);
+    typeEl[_0x37e4[69]] = textString;
     i--;
-    if (textString === _0x3ca8[33]) {
+    if (textString === _0x37e4[33]) {
       flag = false;
       speed *= 8;
       if (index >= 0 && index < 2) {
@@ -196,8 +196,8 @@ const typing = () => {
       }
     }
   } else {
-    textString = contentArr[index][_0x3ca8[68]](0, i);
-    typeEl[_0x3ca8[69]] = textString;
+    textString = contentArr[index][_0x37e4[68]](0, i);
+    typeEl[_0x37e4[69]] = textString;
     i++;
     if (textString === contentArr[index]) {
       flag = true;
@@ -206,43 +206,43 @@ const typing = () => {
   }
 };
 var myVar = setInterval(typing, speed);
-nextButton[_0x3ca8[25]](_0x3ca8[70], () => {
-  const _0xa36ax20 = slide[_0x3ca8[71]];
-  slidesContainer[_0x3ca8[72]] += _0xa36ax20;
+nextButton[_0x37e4[25]](_0x37e4[70], () => {
+  const _0x9df1x20 = slide[_0x37e4[71]];
+  slidesContainer[_0x37e4[72]] += _0x9df1x20;
 });
-prevButton[_0x3ca8[25]](_0x3ca8[70], () => {
-  const _0xa36ax20 = slide[_0x3ca8[71]];
-  slidesContainer[_0x3ca8[72]] -= _0xa36ax20;
+prevButton[_0x37e4[25]](_0x37e4[70], () => {
+  const _0x9df1x20 = slide[_0x37e4[71]];
+  slidesContainer[_0x37e4[72]] -= _0x9df1x20;
 });
-function sendEmail({ _0xa36ax22, _0xa36ax23, _0xa36ax24 }) {
-  fetch(_0x3ca8[81], {
-    method: _0x3ca8[82],
-    mode: _0x3ca8[83],
-    cache: _0x3ca8[84],
-    body: JSON[_0x3ca8[85]]({ name, email, message }),
+function sendEmail({ _0x9df1x22, _0x9df1x23, _0x9df1x24 }) {
+  fetch(_0x37e4[81], {
+    method: _0x37e4[82],
+    mode: _0x37e4[83],
+    cache: _0x37e4[84],
+    body: JSON[_0x37e4[85]]({ name, email, message }),
   })
-    [_0x3ca8[80]](() => {
-      formSubmitStatus[_0x3ca8[57]](_0x3ca8[73], _0x3ca8[78]);
-      formSubmitStatus[_0x3ca8[26]] = _0x3ca8[79];
+    [_0x37e4[80]](() => {
+      formSubmitStatus[_0x37e4[57]](_0x37e4[73], _0x37e4[78]);
+      formSubmitStatus[_0x37e4[26]] = _0x37e4[79];
     })
-    [_0x3ca8[77]]((_0xa36ax25) => {
-      formSubmitStatus[_0x3ca8[57]](_0x3ca8[73], _0x3ca8[74]);
-      formSubmitStatus[_0x3ca8[26]] = _0x3ca8[75];
-      console[_0x3ca8[76]](_0xa36ax25);
+    [_0x37e4[77]]((_0x9df1x25) => {
+      formSubmitStatus[_0x37e4[57]](_0x37e4[73], _0x37e4[74]);
+      formSubmitStatus[_0x37e4[26]] = _0x37e4[75];
+      console[_0x37e4[76]](_0x9df1x25);
     });
 }
-formSubmitBtn[_0x3ca8[25]](_0x3ca8[70], (_0xa36axf) => {
-  _0xa36axf[_0x3ca8[86]]();
-  const _0xa36ax26 = document[_0x3ca8[87]][_0x3ca8[10]];
-  const _0xa36ax27 = new FormData(_0xa36ax26);
-  const _0xa36ax28 = {
-    name: `${_0x3ca8[33]}${_0xa36ax27[_0x3ca8[89]](_0x3ca8[88])}${
-      _0x3ca8[90]
-    }${_0xa36ax27[_0x3ca8[89]](_0x3ca8[91])}${_0x3ca8[33]}`,
-    email: _0xa36ax27[_0x3ca8[89]](_0x3ca8[92]),
-    message: _0xa36ax27[_0x3ca8[89]](_0x3ca8[93]),
+formSubmitBtn[_0x37e4[25]](_0x37e4[70], (_0x9df1xf) => {
+  _0x9df1xf[_0x37e4[86]]();
+  const _0x9df1x26 = document[_0x37e4[87]][_0x37e4[10]];
+  const _0x9df1x27 = new FormData(_0x9df1x26);
+  const _0x9df1x28 = {
+    name: `${_0x37e4[33]}${_0x9df1x27[_0x37e4[89]](_0x37e4[88])}${
+      _0x37e4[90]
+    }${_0x9df1x27[_0x37e4[89]](_0x37e4[91])}${_0x37e4[33]}`,
+    email: _0x9df1x27[_0x37e4[89]](_0x37e4[92]),
+    message: _0x9df1x27[_0x37e4[89]](_0x37e4[93]),
   };
-  sendEmail(_0xa36ax28);
-  console[_0x3ca8[94]](_0xa36ax28);
-  contactForm[_0x3ca8[95]]();
+  sendEmail(_0x9df1x28);
+  console[_0x37e4[94]](_0x9df1x28);
+  contactForm[_0x37e4[95]]();
 });
